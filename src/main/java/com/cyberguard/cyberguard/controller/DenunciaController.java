e6634b90848a26ccb47c2ecb9e9150eb198e0c2d
JustaTBC <tbc@cesar.school>
2026-05-17 21:55:24 -0300

    conectar com o frontend

--- src/main/java/com/cyberguard/cyberguard/controller/DenunciaController.java
+++ src/main/java/com/cyberguard/cyberguard/controller/DenunciaController.java
@@ -1,13 +1,20 @@
 package com.cyberguard.cyberguard.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.cyberguard.cyberguard.entity.Denuncia;
 import com.cyberguard.cyberguard.service.DenunciaService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/denuncias")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DenunciaController {
 
     private final DenunciaService service;
